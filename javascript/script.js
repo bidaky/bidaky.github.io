61f01fe1e64e31d2022f049165352ac4162bb38d
Julia <bidaky@ukr.net>
2021-04-21 22:18:04 +0300

    working sign up and registration
    sign up works  and save data on local storage

--- javascript/script.js
+++ javascript/script.js
@@ -12,54 +12,57 @@ const RegBlock = document.querySelectorAll('.reg-step .progress-bar-block');
 let max = 4;
 let current = 1;
 
-First_next_button.addEventListener('click', function () {
+
+First_next_button.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '-25%';
     RegBlock[current-1].classList.add('active');
     progress[current-1].classList.add('active');
     progressCheck[current-1].classList.add('active');
     current+=1;
 });
-next_buttonFirst.addEventListener('click', function () {
+next_buttonFirst.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '-50%';
     RegBlock[current-1].classList.add('active');
     progress[current-1].classList.add('active');
     progressCheck[current-1].classList.add('active');
     current+=1;
 });
-next_buttonSecond.addEventListener('click', function () {
+next_buttonSecond.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '-75%';
     RegBlock[current-1].classList.add('active');
     progress[current-1].classList.add('active');
     progressCheck[current-1].classList.add('active');
     current+=1;
 });
-submit.addEventListener('click', function () {
+submit.addEventListener('click', function (event) {
     RegBlock[current-1].classList.add('active');
     progress[current-1].classList.add('active');
     progressCheck[current-1].classList.add('active');
     current+=1;
-    setTimeout(function (){
-        alert("Success");
-        location.replace('profile.html')
-    },800)
 });
 
 
-prev_buttonFirst.addEventListener('click', function () {
+prev_buttonFirst.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '0%';
     RegBlock[current-2].classList.remove('active');
     progress[current-2].classList.remove('active');
     progressCheck[current-2].classList.remove('active');
     current-=1;
 });
-prev_buttonSecond.addEventListener('click', function () {
+prev_buttonSecond.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '-25%';
     RegBlock[current-2].classList.remove('active');
     progress[current-2].classList.remove('active');
     progressCheck[current-2].classList.remove('active');
     current-=1;
 });
-prev_buttonThird.addEventListener('click', function () {
+prev_buttonThird.addEventListener('click', function (event) {
+    event.preventDefault();
     slidePage.style.marginLeft = '-50%';
     RegBlock[current-2].classList.remove('active');
     progress[current-2].classList.remove('active');
