f54abdacb8ee182da859668c049be6f4cfa4713c
Julia <bidaky@ukr.net>
2021-05-24 16:01:46 +0300

    lab6-web-socket

---
+++ javascript/sign.js
@@ -0,0 +1,32 @@
+const {signform} = document.forms;
+
+function retrieveFormValue(event){
+    event.preventDefault();
+
+    const {email, password} = signform;
+    const values = {
+        email : email.value,
+        password : password.value
+    };
+
+    const signUrl = 'http://127.0.0.1:5000/user/login?email='+email.value+'&password='+password.value;
+    console.log(signUrl)
+    fetch(signUrl,{method : "GET"} ).then(function(response) {
+        if(response.ok) {
+            response.json().then(function(json) {
+                let data = json;
+                localStorage.setItem('user',  data.user)
+                localStorage.setItem('token',data.basicAuthToken)
+                alert('Success')
+                document.location.replace('profile.html')
+            });
+        } else {
+            alert('Wrong username or password');
+        }
+    })}
+
+signform.addEventListener('submit', retrieveFormValue);
+// var userdata = localStorage.getItem('user')
+// var auth = localStorage.getItem('token')
+// var userdata2 = JSON.parse(userdata)
+// console.log(userdata2, auth)
---
+++ javascript/validator.js
@@ -0,0 +1,21 @@
+document.querySelector('.next-2').onclick = function () {
+    var password = document.querySelector('.password').value,
+        confirmPassword = document.querySelector('.confirm-password').value;
+    if (password === ''){
+        alert("Field cannot be empty.");
+    }
+    else if (password !== confirmPassword){
+        alert("Password didn`t match.");
+        return false;
+    }
+    pass = document.getElementById('pass')
+    if (pass.length < 8){
+
+        alert('Password should contain at least 8 symbols')
+        document.getElementById('password').value = ''
+        document.getElementById('conf-password').value = ''
+        return false;
+    }
+
+
+}
