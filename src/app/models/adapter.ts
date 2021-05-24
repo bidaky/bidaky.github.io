93723618ea7e596c5876de4a5e1e8b06d4ab50e8
Julia <bidaky@ukr.net>
2021-05-24 15:09:47 +0300

    reset changes from lab5

--- src/app/models/adapter.ts
+++
@@ -1,4 +0,0 @@
-export interface Adapter<T, U>
-{
-  adapt(item: T): U;
-}
--- src/app/models/thought-model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ThoughtModel } from './thought-model';
-
-describe('ThoughtModel', () => {
-  it('should create an instance', () => {
-    expect(new ThoughtModel()).toBeTruthy();
-  });
-});
